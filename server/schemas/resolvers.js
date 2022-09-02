ec9b65c770062c5d970be6a0a61f88eb0d2591e9
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-09-02 14:47:00 -0400

    write mutations and savedBookInput

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,12 +8,57 @@ const resolvers = {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
-          .populate("thoughts")
-          .populate("friends");
+          .populate("savedBooks");
 
         return userData;
       }
       throw new AuthenticationError("Not logged in");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parents, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+        throw new AuthenticationError("You need to be logged in!");
+      }
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,16 +16,32 @@ const typeDefs = gql`
     username: String!
     email: String!
     bookCount: Int
-    savedBooks: [Books]
+    savedBooks: [Book]
+  }
+
+  input savedBookInput {
+    description: String
+    title: String!
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
   }
 
   type Query {
-    me: user
+    me: User
   }
 
   type Auth {
     token: ID!
     user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBookInput!): User
+    removeBook(bookId: String!): User
   }
 `;
 
